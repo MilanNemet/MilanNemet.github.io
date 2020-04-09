68b5f28c1d6af2da704565c344df3a6b19b4d2be
MilanNemet <mail.mnemeth@gmail.com>
2020-04-09 22:08:58 +0200

    finish project

--- myTodo/script.js
+++ myTodo/script.js
@@ -1,8 +1,14 @@
 console.debug("Starting script...");
 
+var listItems;
+
 window.onload = function () {
     let buttonAdd = this.document.getElementById("todoButtonAdd");
     let buttonReset = this.document.getElementById("todoButtonReset");
+    let textBox = this.document.getElementById("todoText");
+
+    textBox.addEventListener("focus", (e) => e.target.className = "active")
+    textBox.addEventListener("focusout", (e) => e.target.className = "inactive")
 
     buttonAdd.addEventListener("mousedown", setStyleDown);
     buttonAdd.addEventListener("mouseup", setStyleUpOrOut);
@@ -24,20 +30,45 @@ let setStyleUpOrOut = (e) => {
     e.target.style.boxShadow = "0 0 1px 1px #666";
 }
 let insertNewListItem = () => {
-    let ul = document.getElementById("todoList")
+    let ul = document.getElementById("todoList");
     let input = document.getElementById("todoText");
 
     if (input.value.length > 0) {
         let li = document.createElement("li");
-        let textNode = document.createTextNode(input.value)
+        let textNode = document.createTextNode(input.value);
         li.appendChild(textNode);
         ul.appendChild(li);
         input.value = "";
+
+        listItems = document.querySelectorAll("#todoList > li");
+        for (var i = 0; i < listItems.length; i++) {
+            listItems[i].addEventListener("click", liClickHandler);
+        }
     }
+    else {
+        showErrorMsg("There is nothing to add...");
+    }
+
+    input.focus();
 }
 let resetList = () => {
     let answer = confirm("All list-items will be deleted permanently!\nDo you want to continue?");
     if (answer) {
         document.getElementById("todoList").innerHTML = '';
+    }
+}
+let showErrorMsg = (str) => {
+    let error = document.querySelector(".error");
+    error.innerHTML = str;
+    error.style.visibility = 'visible';
+    setTimeout(function () { error.style.visibility = 'hidden'; }, 4000);
+}
+let liClickHandler = (e) => {
+    let li = e.target;
+    if (li.style.textDecoration != "line-through") {
+        li.style.textDecoration = "line-through";
+    }
+    else {
+        li.style.textDecoration = "none";
     }
 }
