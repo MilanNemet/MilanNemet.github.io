0cb59d2f89f7bbbfa43d577f19556706cc130c79
MilanNemet <mail.mnemeth@gmail.com>
2020-04-09 19:11:03 +0200

    Update script.js

--- myTodo/script.js
+++ myTodo/script.js
@@ -1,1 +1,14 @@
-// JavaScript source code
+window.onload = function () {
+    let button = this.document.getElementById("inputButton");
+    button.addEventListener("mousedown", setStyleDown)
+    button.addEventListener("mouseup", setStyleUp)
+}
+
+let setStyleDown = (e) => {
+    e.style.color = "white";
+    e.style.background = "black";
+}
+let setStyleUp = (e) => {
+    e.style.color = "black";
+    e.style.background = "white";
+}
